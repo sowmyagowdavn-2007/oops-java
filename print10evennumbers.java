b64d29ac495fa856ea6c4a2cc7ba3f82250f301b
Sowmya Shree V N <sowmyagowdavn824@gmail.com>
2026-09-08 08:18:09 +0530

    Add files via upload

---
+++ print10evennumbers.java
@@ -0,0 +1,11 @@
+public class EvenSum {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 2; i <= 20; i += 2) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum of first 10 even numbers = " + sum);
+    }
+}
